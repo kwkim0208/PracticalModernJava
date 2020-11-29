752fc50b676e87f354b22ed5495fc5e2328971f9
kwkim <ruddn0208@gmail.com>
2020-11-30 08:52:42 +0900

    JDK8 training

--- src/ch04/UnaryOperatorExample.java
+++ src/ch04/UnaryOperatorExample.java
@@ -8,5 +8,7 @@ public class UnaryOperatorExample {
         System.out.println(operatorA.apply(1));
         System.out.println(operatorA.apply(2));
         System.out.println(operatorA.apply(3));
+        UnaryOperator<String>operatorB=(String s)->s+" training";
+        System.out.println(operatorB.apply("lambda"));
     }
 }
